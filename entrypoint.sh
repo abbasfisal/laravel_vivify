6de0c0e8ca20d03d468521f6792fac0fbad37bf8
abbas <abbasmomeny1994@gmail.com>
2024-11-10 08:55:16 +0330

    ch 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Start the main process (php-fpm)
-#exec "$@"
+exec "$@"

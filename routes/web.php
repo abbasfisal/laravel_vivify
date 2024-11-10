478632a7660da500080af0fc258f11592935b564
abbas <abbasmomeny1994@gmail.com>
2024-11-10 09:30:44 +0330

    ch 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Book;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/books', function () {
+    Artisan::call('db:seed', ['--class' => 'BookSeeder']);
+    return Book::all();
 });
